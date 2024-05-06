d66d37cf3996814a03fa5b18fb30576a2ac06b0e
AnnyaB <anabordin2008@gmail.com>
2024-05-06 14:50:43 -0300

    Adicionar arquivos de projeto.

---
+++ ListaDinamica/Contact.cs
@@ -0,0 +1,29 @@
+﻿namespace ListaDinamica
+{
+    internal class Contact
+    {
+        string Name;
+        string Phone;
+        Contact Next;
+
+        public Contact(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+            Next = null;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetNext(Contact nextContact) 
+        {
+            Next = nextContact;
+        }
+        public Contact GetNext()
+        {
+            return Next;
+        }
+    }
+}
